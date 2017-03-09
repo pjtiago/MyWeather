cef0179a12dc6c5008843ebdd3730783f9bb6b64
pjota <cheapinfor@mail.com>
2017-03-09 23:05:29 +0000

    Initial commit

---
+++ app/src/main/java/com/estg/pjtiago/myweather/AlertDialogFragment.java
@@ -0,0 +1,39 @@
+package com.estg.pjtiago.myweather;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by PJ on 08/03/2017.
+ */
+
+public class AlertDialogFragment extends android.app.DialogFragment{
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(context.getString(R.string.error_title))
+                .setMessage(context.getString(R.string.error_message))
+                .setPositiveButton(context.getString(R.string.error_ok_button_text),null);
+
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+    /*
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(R.string.error_title)
+                .setMessage(R.string.error_message)
+                .setPositiveButton(R.string.error_ok_button_text, null);
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }`*/
+}
+
